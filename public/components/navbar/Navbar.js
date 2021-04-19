36031b515136d999d5307b676b569c8904fd0f73
Gustavo Riedo <gh03012@gmail.com>
2021-04-19 10:16:43 -0300

    Language switch added

--- public/components/navbar/Navbar.js
+++ public/components/navbar/Navbar.js
@@ -6,6 +6,17 @@ import { i18n } from '../../../src/translate/i18n';
 import style from './Navbar.module.css';
 
 export default class Navbar extends Component {
+   switchLanguage = () => {
+      if (typeof window !== 'undefined') {
+         if (localStorage.getItem('i18nextLng') == 'pt-BR') {
+            localStorage.setItem('i18nextLng', 'en-US');
+         } else if (localStorage.getItem('i18nextLng') == 'en-US') {
+            localStorage.setItem('i18nextLng', 'pt-BR');
+         }
+         window.location = window.location;
+      }
+   };
+
    render() {
       return (
          <nav className={style.navbar}>
@@ -35,6 +46,22 @@ export default class Navbar extends Component {
                         {i18n.t('titles.history')}
                      </a>
                   </Link>
+               </li>
+               <li className={style.navbarItem}>
+                  <a
+                     className={style.navbarLink}
+                     onClick={() => {
+                        this.switchLanguage();
+                     }}
+                  >
+                     <Image
+                        src="/images/language.svg"
+                        height={40}
+                        width={100}
+                        alt="Switch language"
+                     />
+                     {i18n.t('titles.changeLanguage')}
+                  </a>
                </li>
                <li className={style.navbarItem}>
                   <Link href="https://google.com">
